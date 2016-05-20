e8ebac432f6e118d9ec406024602d32fba38c1fd
Wender Lima <wender.lima@gmail.com>
2016-05-20 19:56:24 -0300

    Adding resize option

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ***
 
-AngularJS directive for multiple file upload
+AngularJS directive for multiple file upload, with progress feedback, preview and resize
 
 This directive help you to create a better looking system to control your file uploads using the "Angular Way" with no dependencies, just HTML5 and pure javascript
 
@@ -10,7 +10,7 @@ As a directive, the entire process can be easily customized with your own CSS an
 
 ## Example: 
 ```HTML
-<file-upload class="btn btn-success btn-rad btn-trans file-upload" name="userFile" ng-model="my.foo.model" multiple="false" uri="/upload/upload" accept="image/png, image/jpg">
+<file-upload class="btn btn-success btn-rad btn-trans file-upload" name="userFile" resize="true" max-width="1280" max-height="1280" ng-model="my.foo.model" multiple="false" uri="/upload/upload" accept="image/png, image/jpg">
     <i class="fa fa-upload"></i>
     <span>Upload Files</span>
     <ul>
@@ -36,6 +36,9 @@ This directive will keep it's model update with whole information about selected
 - **credentials**: default 'false' (Share your credentials between your browser and the API)
 - **preview**: default 'false' (Process a base64 preview of the selected image)
 - **file-selector**: default 'false' (By default the whole area is clickable but you may want to select the file targeting some other element inside the directive, uses css selector such as "span.file-buttom")
+- **resize**: default 'false' (Must be used with max-width and max-height)
+- **max-width**: default 'false' (Must be used with resize and max-height)
+- **max-height**: default 'false' (Must be used with resize and max-width)
 
 ## Return
 Charge it's model with an Array of objects
--- src/angular-multiple-file-upload.js
+++ src/angular-multiple-file-upload.js
@@ -17,17 +17,105 @@ angular.module('fileUpload', [])
                     shareCredentials,
                     withPreview,
                     fileSelector,
+                    resize,
+                    maxWidth,
+                    maxHeight,
                     sel;
 
                 fileName = attr.name || 'userFile';
-                shareCredentials = attr.credentials === 'true' ? true : false;
-                withPreview = attr.preview === 'true' ? true : false;
-                fileSelector = angular.isDefined(attr.fileSelector)  ? attr.fileSelector : false;
+                shareCredentials = attr.credentials === 'true';
+                withPreview = attr.preview === 'true';
+                resize = attr.resize === 'true';
+                maxWidth = angular.isDefined(attr.maxWidth) ? parseInt(attr.maxWidth) : false;
+                maxHeight = angular.isDefined(attr.maxHeight) ? parseInt(attr.maxHeight) : false;
+                fileSelector = angular.isDefined(attr.fileSelector) ? attr.fileSelector : false;
 
                 el.append('<input style="display: none !important;" type="file" ' + (attr.multiple == 'true' ? 'multiple' : '') + ' accept="' + (attr.accept ? attr.accept : '') + '" name="' + fileName + '"/>');
                 uri = attr.uri || '/upload/upload';
 
+                function Resize(file, uri, index, type) {
+                    var canvas = document.createElement("canvas");
+                    var img = document.createElement("img");
+                    var reader = new FileReader();
+                    reader.onload = function (e) {
+                        img.src = e.target.result;
+                        draw();
+                    };
+                    reader.readAsDataURL(file);
+
+                    function b64toBlob(b64Data, contentType, sliceSize) {
+                        contentType = contentType || '';
+                        sliceSize = sliceSize || 512;
+
+                        var byteCharacters = atob(b64Data);
+                        var byteArrays = [];
+
+                        for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+                            var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+                            var byteNumbers = new Array(slice.length);
+                            for (var i = 0; i < slice.length; i++) {
+                                byteNumbers[i] = slice.charCodeAt(i);
+                            }
+
+                            var byteArray = new Uint8Array(byteNumbers);
+
+                            byteArrays.push(byteArray);
+                        }
+
+                        var blob = new Blob(byteArrays, {type: contentType});
+                        return blob;
+                    }
+
+                    function draw() {
+                        var ctx = canvas.getContext("2d");
+                        ctx.drawImage(img, 0, 0);
+
+                        var width = img.width;
+                        var height = img.height;
+
+                        if (width > height) {
+                            if (width > maxWidth) {
+                                height *= maxWidth / width;
+                                width = maxWidth;
+                            }
+                        } else {
+                            if (height > maxHeight) {
+                                width *= maxHeight / height;
+                                height = maxHeight;
+                            }
+                        }
+                        canvas.width = width;
+                        canvas.height = height;
+
+                        ctx = canvas.getContext("2d");
+                        ctx.drawImage(img, 0, 0, width, height);
+                        var b64 = canvas.toDataURL(type).split(',')[1];
+                        file = b64toBlob(b64, type, 512);
+                        uploadFile(file, uri, index);
+                    }
+                };
+
+
+                function upload(file, uri, index) {
+                    if (resize && maxWidth && maxHeight && (file.type.indexOf('image/') !== -1)) {
+                        Resize(file, uri, index, file.type);
+                    } else {
+                        uploadFile(file, uri, index);
+                    }
+                    return {
+                        name: file.name,
+                        size: file.size,
+                        type: file.type,
+                        status: {},
+                        percent: 0,
+                        preview: null
+                    }
+
+                }
+
                 function uploadFile(file, uri, index) {
+                    console.log(file, uri, index);
                     var xhr = new XMLHttpRequest(),
                         fd = new FormData(),
                         progress = 0;
@@ -47,6 +135,7 @@ angular.module('fileUpload', [])
                         scope.ngModel[index].percent = progress;
                         scope.$apply();
                     }, false);
+
                     fd.append(fileName, file);
                     xhr.send(fd);
 
@@ -58,19 +147,10 @@ angular.module('fileUpload', [])
                         };
                         reader.readAsDataURL(file);
                     }
-
-                    return {
-                        name: file.name,
-                        size: file.size,
-                        type: file.type,
-                        status: {},
-                        percent: 0,
-                        preview: null
-                    }
                 }
 
-                $timeout(function(){
-                    sel = fileSelector?angular.element(el[0].querySelectorAll(fileSelector)[0]):el;
+                $timeout(function () {
+                    sel = fileSelector ? angular.element(el[0].querySelectorAll(fileSelector)[0]) : el;
                     sel.bind('click', function () {
                         if (!scope.disabled) {
                             scope.$eval(el.find('input')[0].click());
@@ -82,7 +162,7 @@ angular.module('fileUpload', [])
                     var files = e.srcElement.files || e.dataTransfer.files;
                     var list = [];
                     for (var i = 0, f; f = files[i]; i++) {
-                        list.push(uploadFile(f, uri, i));
+                        list.push(upload(f, uri, i));
                     }
                     e.srcElement.files = null;
                     e.srcElement.value = '';
